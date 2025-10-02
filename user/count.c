cd2fe88fa56407f5bc9ae4c132b728f19b3d515b
Pratha Patil <pjpatil@dons.usfca.edu>
2025-10-02 09:57:48 -0700

    adding some error handling

--- user/count.c
+++ user/count.c
@@ -165,8 +165,8 @@ main(int argc, char *argv[])
         word = argv[1];
 
         // for one file/directory
-        if(argc <= 2){
-                count(word, argv[2]);
+        if(argc == 2){
+                count(word, ".");
                 exit(0);
         }
 
